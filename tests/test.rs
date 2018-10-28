aef1e7dd7a3d818d2f139e1328096a94f7d2feb2
Ken Reed <kenakioreed@gmail.com>
2018-10-27 20:31:01 -0400

    fixed tests

--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,8 @@
  #![feature(test)]
 #![feature(trusted_len)]
 extern crate axgeom;
-extern crate dinotree_inner;
-use dinotree_inner::*;
+extern crate dinotree;
+use dinotree::*;
 extern crate compt;
 use compt::*;
 
@@ -17,11 +17,11 @@ fn assert_length<I:std::iter::TrustedLen>(it:I){
 fn test_zero_sized(){
 	let bots=vec![();1];
 
-	let tree=DynTree::new(axgeom::YAXISS,(),&bots,|_b|{
+	let tree=DinoTree::new(axgeom::YAXISS,(),&bots,|_b|{
         axgeom::Rect::new(0isize,0,0,0)
     });
 
-    let n=tree.get_iter().next();
+    let n=tree.vistr().next();
     assert_eq!(n.1.is_none(),true);
     assert_eq!(n.0.range.len(),1);
 
@@ -31,11 +31,11 @@ fn test_zero_sized(){
 fn test_one(){
 	let bots=vec![0usize;1];
 
-	let tree=DynTree::new(axgeom::YAXISS,(),&bots,|_b|{
+	let tree=DinoTree::new(axgeom::YAXISS,(),&bots,|_b|{
         axgeom::Rect::new(0isize,0,0,0)
     });
 
-    let n=tree.get_iter().next();
+    let n=tree.vistr().next();
     assert_eq!(n.1.is_none(),true);
     assert_eq!(n.0.range.len(),1);
 }
@@ -45,14 +45,14 @@ fn test_one(){
 fn test_many(){
     let bots=vec![0usize;1000];
 
-    let tree=DynTree::new(axgeom::YAXISS,(),&bots,|_b|{
+    let tree=DinoTree::new(axgeom::YAXISS,(),&bots,|_b|{
         axgeom::Rect::new(0isize,0,0,0)
     });
 
-    assert_eq!(tree.get_iter().dfs_inorder_iter().count(),tree.get_num_nodes());
+    assert_eq!(tree.vistr().dfs_inorder_iter().count(),tree.num_nodes());
     
     let mut num_div=0;
-    for (_,b) in tree.get_iter().dfs_inorder_iter(){
+    for (_,b) in tree.vistr().dfs_inorder_iter(){
         if let Some(nonleaf)=b{
             if let Some(non_empty)=nonleaf{
                 num_div+=1;
@@ -69,11 +69,11 @@ fn test_many(){
 fn test_empty(){
 	let bots:Vec<()>=Vec::new();
 
-	let tree=DynTree::new(axgeom::YAXISS,(),&bots,|_b|{
+	let tree=DinoTree::new(axgeom::YAXISS,(),&bots,|_b|{
         axgeom::Rect::new(0isize,0,0,0)
     });
 
-    let n=tree.get_iter().next();
+    let n=tree.vistr().next();
     assert_eq!(n.0.range.len(),0);
 }
 
@@ -81,13 +81,13 @@ fn test_empty(){
 fn test_iter(){
 	let bots=vec![0usize;1234];
 
-    let tree=DynTree::new(axgeom::YAXISS,(),&bots,|_b|{
+    let tree=DinoTree::new(axgeom::YAXISS,(),&bots,|_b|{
         axgeom::Rect::new(0isize,0,0,0)
     });
 
 
     let mut last=None;
-    for b in tree.iter_every_bot(){
+    for b in tree.iter(){
     	match last{
     		None=>{
     			last=Some(b as *const BBox<isize,usize>)
@@ -105,12 +105,12 @@ fn test_iter(){
 fn test_iter2(){
 	let bots=vec![0usize;1234];
 
-    let tree=DynTree::new(axgeom::YAXISS,(),&bots,|_b|{
+    let tree=DinoTree::new(axgeom::YAXISS,(),&bots,|_b|{
         axgeom::Rect::new(0isize,0,0,0)
     });
 
     let num_bots=bots.len();
-    assert_eq!(tree.iter_every_bot().count(),num_bots);
+    assert_eq!(tree.iter().count(),num_bots);
 	
 }
 #[test]
@@ -118,24 +118,24 @@ fn test(){
 
 	let bots=vec![0usize;1234];
 
-    let mut tree=DynTree::new(axgeom::YAXISS,(),&bots,|_b|{
+    let mut tree=DinoTree::new(axgeom::YAXISS,(),&bots,|_b|{
         axgeom::Rect::new(0isize,0,0,0)
     });
-    dinotree_inner::advanced::are_invariants_met(&tree).unwrap();
+    dinotree::advanced::are_invariants_met(&tree).unwrap();
 
-	assert_length(tree.get_iter_mut().dfs_preorder_iter());
-	assert_length(tree.get_iter().dfs_preorder_iter());
+	assert_length(tree.vistr_mut().dfs_preorder_iter());
+	assert_length(tree.vistr().dfs_preorder_iter());
 
-	let num_nodes=tree.get_num_nodes();
+	let num_nodes=tree.num_nodes();
 
-	assert_eq!(tree.get_iter_mut().dfs_preorder_iter().size_hint().0,num_nodes);
+	assert_eq!(tree.vistr_mut().dfs_preorder_iter().size_hint().0,num_nodes);
 
-	assert_eq!(tree.get_iter().bfs_iter().size_hint().0,num_nodes);
+	assert_eq!(tree.vistr().bfs_iter().size_hint().0,num_nodes);
 
 
-	recc(tree.get_iter_mut());
+	recc(tree.vistr_mut());
 	//recursively check that the length is correct at each node.
-	fn recc(a:NdIterMut<(),BBox<isize,usize>>){
+	fn recc(a:VistrMut<(),BBox<isize,usize>>){
 		let (_nn,rest)=a.next();
 		match rest{
 			Some((_extra,mut left,mut right))=>{
